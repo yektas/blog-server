eec6c49630bddf04827ed89e90afa752571bf1d2
yektas <ahmetsercanyektas@gmail.com>
2019-09-28 11:39:29 +0300

    basic auth with jwt

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { UserResolver } from './UserResolver';
+import { createConnection } from 'typeorm';
+import cookieParser from 'cookie-parser';
+import { verify } from 'jsonwebtoken';
+import cors from 'cors';
+import { User } from './entity/User';
+import { createAccessToken, createRefreshToken } from './auth';
+import { sendRefreshToken } from './sendRefreshToken';
+
+(async () => {
+	const app = express();
+	app.use(
+		cors({
+			origin: 'http://localhost:3000',
+			credentials: true
+		})
+	);
+	app.get('/', (_, res) => res.send('hello'));
+	app.post('/refresh_token', cookieParser());
+	app.post('/refresh_token', async (req, res) => {
+		const token = req.cookies.jid;
+		if (!token) {
+			return res.send({ ok: false, accessToken: '' });
+		}
+
+		let payload: any = null;
+		try {
+			payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+		} catch (err) {
+			console.log(err);
+			return res.send({ ok: false, accessToken: '' });
+		}
+
+		// token is valid and
+		// we can send back an access token
+		const user = await User.findOne({ id: payload.userId });
+
+		if (!user) {
+			return res.send({ ok: false, accessToken: '' });
+		}
+
+		if (user.tokenVersion !== payload.tokenVersion) {
+			return res.send({ ok: false, accessToken: '' });
+		}
+
+		sendRefreshToken(res, createRefreshToken(user));
+
+		return res.send({ ok: true, accessToken: createAccessToken(user) });
+	});
+
+	await createConnection();
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [UserResolver]
+		}),
+		context: ({ req, res }) => ({ req, res })
+	});
+
+	apolloServer.applyMiddleware({ app, cors: false });
+	app.listen(4000, () => {
+		console.log('Express server started');
+	});
+})();
